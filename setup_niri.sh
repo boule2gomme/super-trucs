e415613b335e035c07081787db5014113a0382cc
Boule de gomme <245409198+boule2gomme@users.noreply.github.com>
2026-02-26 17:10:21 +0100

    Some tweaks
    Signed-off-by: Boule de gomme <245409198+boule2gomme@users.noreply.github.com>

--- setup_niri.sh
+++ setup_niri.sh
@@ -8,13 +8,13 @@ set -euo pipefail
 
 # Ensure home directory exists
 if [ ! -d "$HOME" ]; then
-    echo "Error: Home directory '$HOME_DIR' does not exist."
+    echo "Error: Home directory '$HOME' does not exist."
     exit 1
 fi
 
 BACKUP_DIR="${1:-.}"
 CONFIG_DIR="$HOME/.config"
-WALLPAPER_DIR="$HOME/Pictures/Wallpapers"
+IMAGES_DIR="$HOME/Pictures/"
 SYSTEMD_USER_DIR="$CONFIG_DIR/systemd/user"
 
 # Ensure backup directory exists
@@ -24,23 +24,22 @@ if [ ! -d "$BACKUP_DIR" ]; then
 fi
 
 # Create necessary directories
-mkdir -p "$CONFIG_DIR" "$SYSTEMD_USER_DIR" "$HOME/.swaylock" "$WALLPAPER_DIR"
+mkdir -p "$CONFIG_DIR" "$SYSTEMD_USER_DIR" "$HOME/.swaylock" "$IMAGES_DIR"
 
 # Copy configs
 echo "Copying configs..."
 cp -r "$BACKUP_DIR/fuzzel" "$CONFIG_DIR/"
 cp -r "$BACKUP_DIR/mako" "$CONFIG_DIR/"
 cp -r "$BACKUP_DIR/niri" "$CONFIG_DIR/"
-cp -r "$BACKUP_DIR/swaylock" "$HOME_DIR/.swaylock/"
 cp -r "$BACKUP_DIR/waybar" "$CONFIG_DIR/"
-cp -r "$BACKUP_DIR/waypaper" "$CONFIG_DIR/"
-cp -r "$BACKUP_DIR/images" "$WALLPAPER_DIR"
+cp -r "$BACKUP_DIR/swaylock/." "$HOME/.swaylock/"
+cp -r "$BACKUP_DIR/images/." "$IMAGES_DIR/"
 
 # Copy systemd user services
 echo "Copying systemd user services..."
 cp -r "$BACKUP_DIR/systemd/user/"* "$SYSTEMD_USER_DIR/"
 
-# Create service dependencies 
+# Create service dependencies
 systemctl --user add-wants niri.service mako.service
 systemctl --user add-wants niri.service waybar.service
 systemctl --user add-wants niri.service swayidle.service
@@ -55,4 +54,3 @@ echo "Enabling and starting services..."
 systemctl --user enable --now waybar-restart.path
 
 echo "Setup complete!"
-
